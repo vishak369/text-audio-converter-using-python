2a7f07d087cb7de87a46b9081f4508c03ca0882b
VISHAK <49461325+vishakvijayaraghavan@users.noreply.github.com>
2023-06-29 01:34:21 +0530

    created

---
+++ app.py
@@ -0,0 +1,12 @@
+import pyttsx3
+talk = pyttsx3.init()
+speech = input("say:")
+rate = talk.getProperty('rate')
+print(rate)
+talk.setProperty('rate', 140)
+voices = talk.getProperty('voices')
+talk.setProperty('voice', voices[0].id)
+talk.say(speech)
+talk.runAndWait()
+
+
